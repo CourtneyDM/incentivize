f841cde6ab38648769fc62dd2ada358d8fbb6dd9
Courtney Montgomery <courtney@courtneydm.tech>
2022-07-22 13:06:39 -0500

    added event handler for adding a task

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import Header from "./components/Header";
 
 function App() {
+  const testArr = ["Courtney", "Gisenia", 2, 3, 9, "CJ", "Cams", "Squish"];
+  // Event handler for adding a task
+  const addTask = (e) => {
+    console.log(e.target.value);
+    testArr.splice(0, testArr.length, "Mom & Dad", "Courtney", "Cams", "Cayden");
+    console.log(testArr);
+  };
   return (
     <div className="container">
-      <Header />
+      <Header onClick={addTask} />
     </div>
   );
 }
