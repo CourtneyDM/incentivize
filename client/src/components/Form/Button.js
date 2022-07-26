c6f0480ec36927aebaa7182b07e15d1aaaf669ee
Courtney Montgomery <courtney@courtneydm.tech>
2022-07-25 21:57:34 -0500

    Moved component to Forms folder

---
+++ client/src/components/Form/Button.js
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Form.css";
+
+const Button = ({ color, handleClick, text }) => {
+  return (
+    <button onClick={handleClick} style={{ backgroundColor: color }} className="btn">
+      {text}
+    </button>
+  );
+};
+
+// Set default props
+Button.defaultProps = {
+  color: "steelblue"
+};
+
+// Set PropTypes
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  handleClick: PropTypes.func
+};
+
+export default Button;
