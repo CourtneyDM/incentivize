bd6cb0ee0d8becb8c20c16496b1f43b804192c1c
Courtney Montgomery <courtney@courtneydm.tech>
2022-07-25 22:04:01 -0500

    Refactored for lean code

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,103 @@
 import React from "react";
-import ChildList from "./components/ChildList";
-import Header from "./components/Header";
-import ChoreForm from "./components/ChoreForm";
+import Section from "./components/UI/Section";
+import ChildList from "./components/Child/ChildList";
+import Header from "./components/UI/Header";
+import ChoreForm from "./components/Form/ChoreForm";
 
-function App() {
+const App = () => {
   // Temporary data
   let currentDate = new Date(Date.now());
-  let currentHour = currentDate.getHours();
-  let currentMinute = currentDate.getMinutes();
+  let currentHour, currentMinute, currentSecond;
+
   let childList = [
     {
       name: "Courtney",
       age: 6,
       imgSrc: "",
-      chores: ["Homework/Spanish", "Put away toys", "Eat dinner", "Bath time", "Put on pajamas", "Put away dirty clothes", "Potty time", "Brush teeth", "Choose bedtime story", "Bedtime"]
+      chores: [
+        "Homework/Spanish",
+        "Put away toys",
+        "Eat dinner",
+        "Bath time",
+        "Put on pajamas",
+        "Put away dirty clothes",
+        "Potty time",
+        "Brush teeth",
+        "Choose bedtime story",
+        "Bedtime"
+      ]
     },
     {
       name: "Cameron",
       imgSrc: "",
       age: 3,
-      chores: ["Practice Spanish", "Put away toys", "Eat dinner", "Bath time", "Put on pajamas", "Put away dirty clothes", "Potty time", "Brush teeth", "Choose bedtime story", "Bedtime"]
+      chores: [
+        "Practice Spanish",
+        "Put away toys",
+        "Eat dinner",
+        "Bath time",
+        "Put on pajamas",
+        "Put away dirty clothes",
+        "Potty time",
+        "Brush teeth",
+        "Choose bedtime story",
+        "Bedtime"
+      ]
     },
     {
       name: "Cayden",
       imgSrc: "",
       age: 1,
-      chores: ["Put away toys", "Eat dinner", "Bath time", "Put on pajamas", "Put away dirty clothes", "Potty time", "Brush teeth", "Choose bedtime story", "Bedtime"]
+      chores: [
+        "Put away toys",
+        "Eat dinner",
+        "Bath time",
+        "Put on pajamas",
+        "Put away dirty clothes",
+        "Potty time",
+        "Brush teeth",
+        "Choose bedtime story",
+        "Bedtime"
+      ]
     }
   ];
 
-  const onClick = () => {
-    console.log(`Current Date: ${currentDate}\nCurrent Hour: ${currentHour}\nCurrent Minute: ${currentMinute}`);
-  };
-
+  // Event handler for input field change
   const handleChange = (e) => {
     console.log("Input field was changed.");
   };
 
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(`Current Date: ${currentDate}\nCurrent Hour: ${currentHour}\nCurrent Minute: ${currentMinute}`);
+  };
+
+  const setCurrentTime = () => {
+    currentHour = currentDate.getHours();
+    currentMinute = currentDate.getMinutes();
+    currentSecond = currentDate.getSeconds();
+  };
+
+  // Event handler for toggling chores when completed
+  const toggleChore = (event) => {
+    let chore = event.target;
+    // console.log(chore.classList);
+
+    chore.classList.contains("checked") ? chore.classList.remove("checked") : chore.classList.add("checked");
+  };
+
+  setCurrentTime();
+
   return (
-    <section className="container">
+    <Section className="container" id="page--container">
+      <p id="current-timestamp">
+        Current Time: {currentHour}:{currentMinute}:{currentSecond} <span id="time-icon">icon goes here</span>
+      </p>
       <Header className="header" title="Rewards Tracker" />
-      <ChildList childList={childList} />
-      <ChoreForm handleChange={handleChange} onClick={onClick} />
-    </section>
+      <ChildList childList={childList} toggleChore={toggleChore} />
+      <ChoreForm handleChange={handleChange} handleButtonClick={handleFormSubmit} />
+    </Section>
   );
-}
+};
 
 export default App;
