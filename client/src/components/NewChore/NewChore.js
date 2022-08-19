a0625962d30234ae0464382be5e1bf04125367bf
Courtney Montgomery <courtney@courtneydm.tech>
2022-08-19 05:49:17 -0500

    Form component for adding chores

---
+++ client/src/components/NewChore/NewChore.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Form from "../Form/ChoreForm;";
+
+import "./NewChore.css";
+
+const NewChore = () => {
+  return (
+    <div className="new-chore">
+      <Form />
+    </div>
+  );
+};
+
+export default NewChore;
